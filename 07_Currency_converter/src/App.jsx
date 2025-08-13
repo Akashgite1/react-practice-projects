5b7756d0b1adba930526acbe2d2c3887dd7af415
Akashgite1 <akashgite550@gmail.com>
2025-08-13 17:16:42 +0530

    fix: clean up whitespace in App component

--- 07_Currency_converter/src/App.jsx
+++ 07_Currency_converter/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
       <h1 class="text-3xl font-bold underline text-center"> Hello world!  </h1>
+      
     </>
   )
 }
